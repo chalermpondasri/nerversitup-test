e51d3ef0a93361fca8de64e5bc210ea949420d7e
Chalermpon Dasri <chalermpon@ucconnect.co.th>
2025-03-04 17:29:56 +0700

    add odd number

--- odd-number.spec.ts
+++ odd-number.spec.ts
@@ -1,1 +1,40 @@
 // Write your test here
+import { findOddNumber } from './odd-number'
+
+describe('Odd number', () => {
+    it('should return 7', () => {
+        const expectedResult = 7
+        const result = findOddNumber([7])
+        expect(result).toEqual(expectedResult)
+    })
+
+    it('should return 0', () => {
+        const expectedResult = 0
+        const result = findOddNumber([0])
+        expect(result).toEqual(expectedResult)
+    })
+
+    it('should return 2', () => {
+        const expectedResult = 2
+        const result = findOddNumber([1,1,2])
+        expect(result).toEqual(expectedResult)
+    })
+
+    it('should return 0', () => {
+        const expectedResult = 0
+        const result = findOddNumber([0,1,0,1,0])
+        expect(result).toEqual(expectedResult)
+    })
+
+    it('should return 4', () => {
+        const expectedResult = 4
+        const result = findOddNumber([1,2,2,3,3,3,4,3,3,3,2,2,1])
+        expect(result).toEqual(expectedResult)
+    })
+
+    it('should return 1', () => {
+        const expectedResult = 1
+        const result = findOddNumber([1,2,2,1,3,3,1,2,3,3,1,2,1])
+        expect(result).toEqual(expectedResult)
+    })
+})
--- odd-number.ts
+++ odd-number.ts
@@ -1,9 +1,21 @@
 /**
- * 
+ *
  * @param numbers list of numbers example [7], [0], [1,1,2], [0,1,0,0,1], [1,2,3,4,5,6,7,8,9,0]
  * @returns one number that odd number
- */ 
-export function findOddNumber(numbers: number[]) : number {
-    // TODO : Start your code here
-    throw new Error("Not implemented");
+ */
+export function findOddNumber(numbers: number[]): number {
+    const map = numbers.reduce((acc, currentValue) => {
+        acc.set(currentValue, (acc.get(currentValue) ?? 0) + 1)
+        return acc
+    }, new Map<number, number>())
+
+    let odd = null
+    for (const mapElement of map) {
+        if(mapElement[1] % 2 !== 0) {
+            odd = mapElement[0]
+            break
+        }
+    }
+
+    return odd
 }
